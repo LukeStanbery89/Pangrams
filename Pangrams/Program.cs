1c3d3bac4ea5e9a84f05b5611cd2fe1c8ab9c0c7
Luke Stanbery <lukestanbery89@gmail.com>
2014-11-02 10:56:11 -0600

    Added a loop to allow the user to enter input multiple times without re-running the program.

--- Pangrams/Program.cs
+++ Pangrams/Program.cs
@@ -8,6 +8,7 @@ namespace Pangrams {
     class Program {
         static void Main(string[] args) {
             bool containsEveryLetter = true;
+            bool goAgain = true;
             
             Console.WriteLine("               -- PANGRAMS --");
             Console.WriteLine("Enter a phrase and this program will tell you if");
@@ -15,22 +16,35 @@ namespace Pangrams {
             Console.WriteLine("------------------------------------------------");
             Console.WriteLine();
 
-            Console.WriteLine("Type a string of text: ");
-            string input = Console.ReadLine();
+            do {
+                Console.WriteLine("Type a string of text: ");
+                string input = Console.ReadLine();
 
-            for(uint i = 0; i < 26; i++) {
-                if(!input.ToLower().Contains((char)((int)'a' + i))) {
-                    containsEveryLetter = false;
-                    break;
+                for(uint i = 0; i < 26; i++) {
+                    if(!input.ToLower().Contains((char)((int)'a' + i))) {
+                        containsEveryLetter = false;
+                        break;
+                    }
                 }
-            }
 
-            Console.WriteLine();
-            if(containsEveryLetter) {
-                Console.WriteLine("This sentence DOES contain every letter.");
-            } else {
-                Console.WriteLine("This sentence DOES NOT contain every letter;");
-            }
+                Console.WriteLine();
+                if(containsEveryLetter) {
+                    Console.WriteLine("This sentence DOES contain every letter.");
+                } else {
+                    Console.WriteLine("This sentence DOES NOT contain every letter;");
+                }
+
+                Console.WriteLine();
+                Console.Write("Go again?");
+                string goAgainSelection = Console.ReadLine();
+                if(goAgainSelection.ToLower() == "y" || goAgainSelection.ToLower() == "yes"){
+                    goAgain = true;
+                }else{
+                    goAgain = false;
+                }
+
+                Console.WriteLine();
+            } while(goAgain);
         }
     }
 }
